9824e6ca2b090092cfa0d9fc28a99fc24bd8b4e7
janis070487 <janis070487@gmail.com>
2021-11-21 00:12:13 +0200

    version1.1

--- Paint.cs
+++ Paint.cs
@@ -2,12 +2,12 @@
 
 public static class Pain
 {
+
     public static char[] Line(double x1, double y1, double x2, double y2 , double accuracy)
     {
-        char[] data = new char[]
-        {
-
-        };
-        return data;
+        
+        char[] dataLine = new char[0];
+        
+        return dataLine;
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,15 @@ namespace Gcode
     {
         static void Main(string[] args)
         {
-          
 
+            Point point = new Point();
+           // poin = 1.0;
             string path = "test_gcode";
 
             File file = new File();
             file.add(Start.start());
             file.add(Move.UpZ());
+
             file.save(path);
         }
     }
